c72dfc0df623fa2c488781cb17fb74391d8783e2
Gert-Jan <gertjancom@gmail.com>
2018-12-27 22:11:16 +0100

    Include action in debug logging

--- gbridge_client.py
+++ gbridge_client.py
@@ -60,7 +60,7 @@ class gBridgeClient:
 
     def callAPI(self, request, action):
         try:
-            Domoticz.Debug('Calling API: %s' % str(request.get_full_url()))
+            Domoticz.Debug('Calling API for action: %s, url: %s' % (action, str(request.get_full_url())))
             response = urllib.request.urlopen(request).read().decode('utf-8')
         except HTTPError as e:
             Domoticz.Error('The server couldn\'t fulfill the request: %s.' % action)
