bcfa695d38ce07690686db002d55f79650fc1d80
Gert-Jan <gertjancom@gmail.com>
2019-04-09 20:38:02 +0200

    Add support for setting the colour of a light

--- adapters/dimmable_adapter.py
+++ adapters/dimmable_adapter.py
@@ -16,15 +16,26 @@ class DimmableAdapter(OnOffSwitchAdapter):
             OnOffSwitchAdapter.callDomoticzApi(self, domoticz_port, urllib.parse.urlencode(params))
         elif action == 'onoff':
             OnOffSwitchAdapter.handleMqttMessage(self, device, data, action, domoticz_port)
+        elif action == 'colorsettingrgb':
+            params = {
+                'param': 'setcolbrightnessvalue',
+                'idx': device['idx'],
+                'hex': data
+            }
+            OnOffSwitchAdapter.callDomoticzApi(self, domoticz_port, urllib.parse.urlencode(params))
 
     def getTraits(self):
-        return [1, 2]
+        return [1, 2, 12]
 
     def publishStateFromDomoticzTopic(self, mqtt_client, device, base_topic, message):
         OnOffSwitchAdapter.publishStateFromDomoticzTopic(self, mqtt_client, device, base_topic, message)
+        base_topic = base_topic + '/' + str(message['idx'])
         if message.get('svalue1') is not None:
-            base_topic = base_topic + '/' + str(message['idx'])
             mqtt_client.Publish(base_topic + '/brightness/set', str(message['svalue1']))
+        if message.get('Color') is not None:
+            rgb_color = message['Color']
+            color = '%02x%02x%02x' % (rgb_color['r'], rgb_color['g'], rgb_color['b'])
+            mqtt_client.Publish(base_topic + '/colorsettingrgb/set', color)
 
     def publishState(self, mqtt_client, device, base_topic, value):
         OnOffSwitchAdapter.publishState(self, mqtt_client, device, base_topic, value)
